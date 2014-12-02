784209043af6860d8ddb0f1bde2f4215d4ecb44a
duytienuit <duytiendtit@gmail.com>
2014-12-02 13:17:16 +0700

    02/12

--- StockeAnalysisApplication/StockeAnalysisApplication/frmStockCluster.Designer.cs
+++ StockeAnalysisApplication/StockeAnalysisApplication/frmStockCluster.Designer.cs
@@ -29,45 +29,35 @@
         private void InitializeComponent()
         {
             this.btnAnalysis = new DevExpress.XtraEditors.SimpleButton();
-            this.cmdClusterAlgorithm = new DevExpress.XtraEditors.ComboBoxEdit();
             this.Algorithm = new DevExpress.XtraEditors.LabelControl();
             this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
-            this.cmdDistanceAlgorithm = new DevExpress.XtraEditors.ComboBoxEdit();
             this.cklStockList = new System.Windows.Forms.CheckedListBox();
             this.btnCheckAll = new DevExpress.XtraEditors.SimpleButton();
             this.btnSeeChart = new DevExpress.XtraEditors.SimpleButton();
             this.txtSearch = new DevExpress.XtraEditors.TextEdit();
             this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
             this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
-            ((System.ComponentModel.ISupportInitialize)(this.cmdClusterAlgorithm.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.cmdDistanceAlgorithm.Properties)).BeginInit();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
             ((System.ComponentModel.ISupportInitialize)(this.txtSearch.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // btnAnalysis
             // 
-            this.btnAnalysis.Location = new System.Drawing.Point(358, 30);
+            this.btnAnalysis.Location = new System.Drawing.Point(12, 350);
             this.btnAnalysis.Name = "btnAnalysis";
-            this.btnAnalysis.Size = new System.Drawing.Size(95, 20);
+            this.btnAnalysis.Size = new System.Drawing.Size(101, 20);
             this.btnAnalysis.TabIndex = 1;
             this.btnAnalysis.Text = "Analysis Cluster";
             this.btnAnalysis.Click += new System.EventHandler(this.btnAnalysis_Click);
             // 
-            // cmdClusterAlgorithm
-            // 
-            this.cmdClusterAlgorithm.Location = new System.Drawing.Point(3, 30);
-            this.cmdClusterAlgorithm.Name = "cmdClusterAlgorithm";
-            this.cmdClusterAlgorithm.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
-            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
-            this.cmdClusterAlgorithm.Properties.Items.AddRange(new object[] {
-            "Hierarchycial"});
-            this.cmdClusterAlgorithm.Size = new System.Drawing.Size(149, 20);
-            this.cmdClusterAlgorithm.TabIndex = 2;
-            // 
             // Algorithm
             // 
-            this.Algorithm.Location = new System.Drawing.Point(3, 11);
+            this.Algorithm.Location = new System.Drawing.Point(506, 11);
             this.Algorithm.Name = "Algorithm";
             this.Algorithm.Size = new System.Drawing.Size(82, 13);
             this.Algorithm.TabIndex = 3;
@@ -75,23 +65,11 @@
             // 
             // labelControl1
             // 
-            this.labelControl1.Location = new System.Drawing.Point(192, 11);
+            this.labelControl1.Location = new System.Drawing.Point(677, 11);
             this.labelControl1.Name = "labelControl1";
             this.labelControl1.Size = new System.Drawing.Size(89, 13);
             this.labelControl1.TabIndex = 5;
             this.labelControl1.Text = "Distance Algorithm";
-            // 
-            // cmdDistanceAlgorithm
-            // 
-            this.cmdDistanceAlgorithm.Location = new System.Drawing.Point(192, 30);
-            this.cmdDistanceAlgorithm.Name = "cmdDistanceAlgorithm";
-            this.cmdDistanceAlgorithm.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
-            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
-            this.cmdDistanceAlgorithm.Properties.Items.AddRange(new object[] {
-            "Euclipdean",
-            "DTW"});
-            this.cmdDistanceAlgorithm.Size = new System.Drawing.Size(149, 20);
-            this.cmdDistanceAlgorithm.TabIndex = 4;
             // 
             // cklStockList
             // 
@@ -105,14 +83,14 @@
             "Stock 6",
             "Stock 7",
             "..."});
-            this.cklStockList.Location = new System.Drawing.Point(-2, 130);
+            this.cklStockList.Location = new System.Drawing.Point(12, 130);
             this.cklStockList.Name = "cklStockList";
-            this.cklStockList.Size = new System.Drawing.Size(100, 154);
+            this.cklStockList.Size = new System.Drawing.Size(100, 214);
             this.cklStockList.TabIndex = 6;
             // 
             // btnCheckAll
             // 
-            this.btnCheckAll.Location = new System.Drawing.Point(-2, 101);
+            this.btnCheckAll.Location = new System.Drawing.Point(13, 101);
             this.btnCheckAll.Name = "btnCheckAll";
             this.btnCheckAll.Size = new System.Drawing.Size(100, 23);
             this.btnCheckAll.TabIndex = 7;
@@ -120,23 +98,23 @@
             // 
             // btnSeeChart
             // 
-            this.btnSeeChart.Location = new System.Drawing.Point(0, 290);
+            this.btnSeeChart.Location = new System.Drawing.Point(12, 376);
             this.btnSeeChart.Name = "btnSeeChart";
-            this.btnSeeChart.Size = new System.Drawing.Size(100, 23);
+            this.btnSeeChart.Size = new System.Drawing.Size(101, 23);
             this.btnSeeChart.TabIndex = 8;
             this.btnSeeChart.Text = "See chart";
             this.btnSeeChart.Click += new System.EventHandler(this.btnSeeChart_Click);
             // 
             // txtSearch
             // 
-            this.txtSearch.Location = new System.Drawing.Point(0, 75);
+            this.txtSearch.Location = new System.Drawing.Point(12, 75);
             this.txtSearch.Name = "txtSearch";
             this.txtSearch.Size = new System.Drawing.Size(100, 20);
             this.txtSearch.TabIndex = 9;
             // 
             // labelControl2
             // 
-            this.labelControl2.Location = new System.Drawing.Point(3, 56);
+            this.labelControl2.Location = new System.Drawing.Point(12, 56);
             this.labelControl2.Name = "labelControl2";
             this.labelControl2.Size = new System.Drawing.Size(24, 13);
             this.labelControl2.TabIndex = 10;
@@ -145,33 +123,67 @@
             // panelControl1
             // 
             this.panelControl1.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.Style3D;
-            this.panelControl1.Location = new System.Drawing.Point(124, 78);
+            this.panelControl1.Location = new System.Drawing.Point(151, 78);
             this.panelControl1.Name = "panelControl1";
-            this.panelControl1.Size = new System.Drawing.Size(642, 319);
+            this.panelControl1.Size = new System.Drawing.Size(615, 255);
             this.panelControl1.TabIndex = 11;
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Appearance.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold);
+            this.labelControl3.Location = new System.Drawing.Point(486, 33);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(127, 13);
+            this.labelControl3.TabIndex = 12;
+            this.labelControl3.Text = "Hierarchical Clustering";
+            // 
+            // labelControl4
+            // 
+            this.labelControl4.Appearance.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold);
+            this.labelControl4.Location = new System.Drawing.Point(646, 33);
+            this.labelControl4.Name = "labelControl4";
+            this.labelControl4.Size = new System.Drawing.Size(130, 13);
+            this.labelControl4.TabIndex = 13;
+            this.labelControl4.Text = "Dynamic Time Warping";
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.Location = new System.Drawing.Point(12, 14);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Size = new System.Drawing.Size(135, 20);
+            this.textEdit1.TabIndex = 14;
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(166, 12);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton1.TabIndex = 15;
+            this.simpleButton1.Text = "Import File";
             // 
             // frmStockCluster
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(788, 468);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.textEdit1);
+            this.Controls.Add(this.btnSeeChart);
+            this.Controls.Add(this.labelControl4);
+            this.Controls.Add(this.labelControl3);
             this.Controls.Add(this.panelControl1);
             this.Controls.Add(this.labelControl2);
             this.Controls.Add(this.txtSearch);
-            this.Controls.Add(this.btnSeeChart);
             this.Controls.Add(this.btnCheckAll);
             this.Controls.Add(this.cklStockList);
             this.Controls.Add(this.labelControl1);
-            this.Controls.Add(this.cmdDistanceAlgorithm);
             this.Controls.Add(this.Algorithm);
-            this.Controls.Add(this.cmdClusterAlgorithm);
             this.Controls.Add(this.btnAnalysis);
             this.Name = "frmStockCluster";
             this.Text = "Stock Cluster Program";
-            ((System.ComponentModel.ISupportInitialize)(this.cmdClusterAlgorithm.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.cmdDistanceAlgorithm.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.txtSearch.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -180,16 +192,18 @@
         #endregion
 
         private DevExpress.XtraEditors.SimpleButton btnAnalysis;
-        private DevExpress.XtraEditors.ComboBoxEdit cmdClusterAlgorithm;
         private DevExpress.XtraEditors.LabelControl Algorithm;
         private DevExpress.XtraEditors.LabelControl labelControl1;
-        private DevExpress.XtraEditors.ComboBoxEdit cmdDistanceAlgorithm;
         private System.Windows.Forms.CheckedListBox cklStockList;
         private DevExpress.XtraEditors.SimpleButton btnCheckAll;
         private DevExpress.XtraEditors.SimpleButton btnSeeChart;
         private DevExpress.XtraEditors.TextEdit txtSearch;
         private DevExpress.XtraEditors.LabelControl labelControl2;
         private DevExpress.XtraEditors.PanelControl panelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.LabelControl labelControl4;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
 
     }
 }
